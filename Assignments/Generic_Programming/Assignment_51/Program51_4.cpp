28b647a6341fedbb2540bb6aafbc5ae91277da0d
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-09 12:46:09 +0530

    Search Last occurrence

---
+++ Assignments/Generic_Programming/Assignment_51/Program51_4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+
+int SearchFirst(T *Arr, int iSize, T No)
+{
+    int iCnt = 0, iCount = 0;
+    
+    for(iCnt = iSize; iCnt > 0; iCnt--)
+    {
+        if(Arr[iCnt] == No)
+        {
+            return iCnt + 1;
+        }
+    }
+
+    return -1;
+}
+
+
+int main()
+{
+    int Arr[] = {10,20,30,10,30,40,10,40,10};
+    int iRet = SearchFirst(Arr,9,40);
+
+    if(iRet == -1)
+    {
+        cout<<"Given number is not present\n";
+    }
+    else
+    {
+        cout<<"Last occurence at "<<iRet<<"th position\n";
+    }
+
+    return 0;
+}
