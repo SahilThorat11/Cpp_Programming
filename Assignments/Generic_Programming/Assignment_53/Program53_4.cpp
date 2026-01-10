3978b8d4d09b9aa7e76b83f4086ffb78420e0ea2
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-10 15:46:20 +0530

    Generic program to find second smallest element

---
+++ Assignments/Generic_Programming/Assignment_53/Program53_4.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Array
+{
+    private:
+        T *Arr = NULL;
+        int iSize = 0;
+
+    public:
+        Array(int iValue)
+        {
+            iSize = iValue;
+            Arr = new T[iSize];
+        }
+
+        void Accept()
+        {
+            int iCnt = 0;
+
+            cout << "Enter the elements:\n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cin >> Arr[iCnt];
+            }
+        }
+
+        void Display()
+        {
+            int iCnt = 0;
+
+            cout << "Elements from the Array are:\n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cout << Arr[iCnt] << "\t";
+            }
+            cout << "\n\n";
+        }
+
+        T SecondMin()
+        {
+            int iCnt = 0;
+
+            T Min1 = Arr[0];
+            T Min2 = Arr[1];
+
+            for(iCnt = 1; iCnt < iSize; iCnt++)
+            {
+                if(Arr[iCnt] < Min1)
+                {
+                    Min2 = Min1;
+                    Min1 = Arr[iCnt];
+                }
+                else if(Arr[iCnt] < Min2 && Arr[iCnt] != Min1)
+                {
+                    Min2 = Arr[iCnt];
+                }
+            }
+
+            return Min2;
+        }
+};
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout << "Enter number of elements that you want to enter: "; 
+    cin >> iValue;
+
+    Array<int> aobj = (iValue);
+
+    aobj.Accept();
+    aobj.Display();
+
+    iRet = aobj.SecondMin();
+
+    cout << "Second Minimum number from the array is : " << iRet << "\n";
+
+    return 0;
+}
