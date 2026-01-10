4fdbe008dbc8050c6e9a557b33066f4a3076978d
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-10 15:46:04 +0530

    Generic program to find second largest element

---
+++ Assignments/Generic_Programming/Assignment_53/Program53_3.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Array
+{
+    private:
+        T *Arr = NULL;
+        int iSize = 0;
+
+    public:
+        Array(int iValue)
+        {
+            iSize = iValue;
+            Arr = new T[iSize];
+        }
+
+        void Accept()
+        {
+            int iCnt = 0;
+
+            cout << "Enter the elements:\n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cin >> Arr[iCnt];
+            }
+        }
+
+        void Display()
+        {
+            int iCnt = 0;
+
+            cout << "Elements from the Array are:\n";
+
+            for(iCnt = 0; iCnt < iSize; iCnt++)
+            {
+                cout << Arr[iCnt] << "\t";
+            }
+            cout << "\n\n";
+        }
+
+        T SecondMax()
+        {
+            int iCnt = 0;
+
+            T Max1 = Arr[0];
+            T Max2 = Arr[0];
+
+            for(iCnt = 1; iCnt < iSize; iCnt++)
+            {
+                if(Arr[iCnt] > Max1)
+                {
+                    Max2 = Max1;
+                    Max1 = Arr[iCnt];
+                }
+                else if(Arr[iCnt] > Max2 && Arr[iCnt] != Max1)
+                {
+                    Max2 = Arr[iCnt];
+                }
+            }
+
+            return Max2;
+        }
+
+};
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout << "Enter number of elements that you want to enter: "; 
+    cin >> iValue;
+
+    Array<int> aobj(iValue);
+
+    aobj.Accept();
+    aobj.Display();
+
+    iRet = aobj.SecondMax();
+
+    cout << "Second Maximum number from the array is : " << iRet << "\n";
+
+    return 0;
+}
