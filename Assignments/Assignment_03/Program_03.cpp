11fb4f0f9fbbb937093276d562e6049fd301fb09
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-17 22:52:23 +0530

    Program to display Even factors of given number

---
+++ Assignments/Assignment_03/Program_03.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+DispalyEvenFactors(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo / 2; iCnt++)
+    {
+        if((iNo % iCnt == 0) && (iCnt % 2 == 0))
+        {
+            cout << iCnt << "\t";
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout << "Enter the number : ";
+    cin >> iValue;
+
+    DispalyEvenFactors(iValue);
+
+    return 0;
+}
