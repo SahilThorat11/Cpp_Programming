f5cbc7d85b57178ba694dbb2c0daed1e9448ddea
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-17 22:28:58 +0530

    Program to Display Hello $ Demo with condition check

---
+++ Assignments/Assignment_02/Program_03.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    if(iNo <= 10)
+    {
+        cout << "Hello";
+    }
+    else
+    {
+        cout << "Demo";
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout << "Enter the number : \n";
+    cin >> iValue;
+
+    Display(iValue);
+
+    return 0;
+}
