05f307ec814082de171937406ff9f72c8581c65d
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-17 22:27:41 +0530

    Program to display * on Screen

---
+++ Assignments/Assignment_02/Program_01.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo != iCnt)
+    {
+        cout << "*\t";
+        iCnt++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout << "Enter the number : \n";
+    cin >> iValue;
+
+    Display(iValue);
+
+    return 0;
+}
